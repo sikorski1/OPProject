45a7a5127e9aaf5084c6b64caec99a9d75955041
Kubek-3 <jploskonka8@gmail.com>
2023-12-27 21:49:30 +0100

    chyba git

--- src/projekt/Gui.java
+++ src/projekt/Gui.java
@@ -1,77 +1,60 @@
 package projekt;
 
-import java.awt.BorderLayout;
-import java.awt.Color;
-
-import javax.swing.ImageIcon;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTable;
+import java.awt.*;
+import javax.swing.*;
 
 public class Gui {
-	
+	static String image1 = "piggy_logo.png";
+	static String[] currencyExchanges = {"Kantor Exchagne", "Kantor Baksy", "Kantor Grosz"};
+	static String[] columnNames = {"Currency", "Purchase", "Sale"};
+	static Color[] colors = {Color.red, Color.green, Color.blue};
+	static String[][] tableData = {
+			{"EUR", "4.35", "4.33"},
+			{"USD", "4.12", "4.10"},
+			{"GBP", "4.93", "4.95"}
+
+	};
+	static int width = 1500;
+	static int height = 800;
 	public static void createAndShowGUI() {
+		int numOfCrrExc = currencyExchanges.length;
+		int panelWidth = width/numOfCrrExc;
+
 		JFrame frame = new JFrame();
 		frame.setTitle("Kursy walut");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(1500, 800);
+		frame.setSize(width, height);
 		frame.setVisible(true);
 		frame.setLayout(null);
-;
-		JPanel exchangePanel = new JPanel();
-		exchangePanel.setBackground(Color.red);
-		exchangePanel.setBounds(0, 0, 500, 800);
-		exchangePanel.setLayout(new BorderLayout());
-		JPanel baksyPanel = new JPanel();
-		baksyPanel.setBackground(Color.green);
-		baksyPanel.setBounds(500, 0, 500, 800);
-		baksyPanel.setLayout(new BorderLayout());
-		JPanel groszPanel = new JPanel();
-		groszPanel.setBackground(Color.blue);
-		groszPanel.setBounds(1000, 0, 500, 800);
-		groszPanel.setLayout(new BorderLayout());
-		
-		
-		ImageIcon image = new ImageIcon("piggy_logo.png");
-		frame.setIconImage(image.getImage());
-		
-		
-		String tableData[][] = {
-				{"EUR", "4.35", "4.33"},
-				{"USD", "4.12", "4.10"},
-				{"GBP", "4.93", "4.95"}
-				
-		};
-		
-		String columnNames[] = {"Currency", "Purchase", "Sale"};
-		
-		JTable exchangeTable = new JTable(tableData, columnNames);
-		exchangeTable.setEnabled(false);
-		JScrollPane exchangeTableSP = new JScrollPane(exchangeTable);
-        exchangePanel.add(exchangeTableSP, BorderLayout.NORTH);
-        
-		JTable baksyTable = new JTable(tableData, columnNames);
-		baksyTable.setEnabled(false);
-		JScrollPane baksyTableSP = new JScrollPane(baksyTable);
-        baksyPanel.add(baksyTableSP, BorderLayout.NORTH);
-        
-		JTable groszTable = new JTable(tableData, columnNames);
-		groszTable.setEnabled(false);
-		JScrollPane groszTableSP = new JScrollPane(groszTable);
-        groszPanel.add(groszTableSP, BorderLayout.NORTH);
-        
-        frame.add(exchangePanel);
-        frame.add(baksyPanel);
-        frame.add(groszPanel);
-        
-        
-	}
-	
-    public static void main(String[] args) {
-        javax.swing.SwingUtilities.invokeLater(new Runnable() {
-            public void run() { createAndShowGUI(); }
-        });        
-    }
 
+		int i = 0;
+		int x = 0;
+		for (String crrExc : currencyExchanges) {
+			JPanel panel = new JPanel();
+			JTextField textField = new JTextField(crrExc);
+			textField.setEditable(false);
+			textField.setBackground(Color.white);
+			textField.setBounds(x, 1, panelWidth, 50);
+			frame.add(textField, BorderLayout.NORTH);
+			panel.setBackground(colors[i]);
+			panel.setBounds(x, 51, panelWidth, height - 50);
+			panel.setLayout(new BorderLayout());
+			JTable table = new JTable(tableData, columnNames);
+			table.setEnabled(false);
+			JScrollPane scrollPane = new JScrollPane(table);
+			panel.add(scrollPane, BorderLayout.NORTH);
+			frame.add(panel);
+			i += 1;
+			x += panelWidth;
+		}
+
+		frame.setLocationRelativeTo(null); // Center the frame
+		frame.setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+			public void run() { createAndShowGUI(); }
+		});
+	}
 }

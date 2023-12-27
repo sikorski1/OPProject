23ee62c26adb5a6ce7217aad94ce6c1311da148c
Kacper <kacpersikora.praca@gmail.com>
2023-12-27 11:32:02 +0100

    changes

--- src/projekt/Gui.java
+++ src/projekt/Gui.java
@@ -1,10 +1,25 @@
 package projekt;
 
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+
 public class Gui {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	public static void createAndShowGUI() {
+		JFrame frame = new JFrame();
+		frame.setTitle("Kursy walut");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(1600, 800);
+		frame.setVisible(true);
+		
+		ImageIcon image = new ImageIcon("piggy_logo.png");
+		frame.setIconImage(image.getImage());
 	}
+	
+    public static void main(String[] args) {
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() { createAndShowGUI(); }
+        });        
+    }
 
 }

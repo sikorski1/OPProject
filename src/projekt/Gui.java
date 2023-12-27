24fe804f3bc1c9426657a6fa5530b469364fa83b
Kacper <kacpersikora.praca@gmail.com>
2023-12-27 13:24:07 +0100

    nwm jak to wysrodkowac

--- src/projekt/Gui.java
+++ src/projekt/Gui.java
@@ -1,7 +1,13 @@
 package projekt;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 
 public class Gui {
 	
@@ -9,11 +15,57 @@ public class Gui {
 		JFrame frame = new JFrame();
 		frame.setTitle("Kursy walut");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(1600, 800);
+		frame.setSize(1500, 800);
 		frame.setVisible(true);
+		frame.setLayout(null);
+;
+		JPanel exchangePanel = new JPanel();
+		exchangePanel.setBackground(Color.red);
+		exchangePanel.setBounds(0, 0, 500, 800);
+		exchangePanel.setLayout(new BorderLayout());
+		JPanel baksyPanel = new JPanel();
+		baksyPanel.setBackground(Color.green);
+		baksyPanel.setBounds(500, 0, 500, 800);
+		baksyPanel.setLayout(new BorderLayout());
+		JPanel groszPanel = new JPanel();
+		groszPanel.setBackground(Color.blue);
+		groszPanel.setBounds(1000, 0, 500, 800);
+		groszPanel.setLayout(new BorderLayout());
+		
 		
 		ImageIcon image = new ImageIcon("piggy_logo.png");
 		frame.setIconImage(image.getImage());
+		
+		
+		String tableData[][] = {
+				{"EUR", "4.35", "4.33"},
+				{"USD", "4.12", "4.10"},
+				{"GBP", "4.93", "4.95"}
+				
+		};
+		
+		String columnNames[] = {"Currency", "Purchase", "Sale"};
+		
+		JTable exchangeTable = new JTable(tableData, columnNames);
+		exchangeTable.setEnabled(false);
+		JScrollPane exchangeTableSP = new JScrollPane(exchangeTable);
+        exchangePanel.add(exchangeTableSP, BorderLayout.NORTH);
+        
+		JTable baksyTable = new JTable(tableData, columnNames);
+		baksyTable.setEnabled(false);
+		JScrollPane baksyTableSP = new JScrollPane(baksyTable);
+        baksyPanel.add(baksyTableSP, BorderLayout.NORTH);
+        
+		JTable groszTable = new JTable(tableData, columnNames);
+		groszTable.setEnabled(false);
+		JScrollPane groszTableSP = new JScrollPane(groszTable);
+        groszPanel.add(groszTableSP, BorderLayout.NORTH);
+        
+        frame.add(exchangePanel);
+        frame.add(baksyPanel);
+        frame.add(groszPanel);
+        
+        
 	}
 	
     public static void main(String[] args) {
